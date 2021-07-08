21881e9eb13249c6a4d45534521167a8e684f826
Leonardo Silva <leonardo.sbarbosa2801@gmail.com>
2021-07-08 15:07:24 -0300

    Exercicio 01

---
+++ 01-exercicio.rb
@@ -0,0 +1,15 @@
+puts "Quantas horas há em um ano?"
+puts "Um ano possui " + (24*365).to_s + " horas", ""
+
+puts "Quantos minutos há em uma década?"
+puts "Uma década possui " + (60*24*365*10).to_s + " minutos", ""
+
+puts "Qual é a sua idade em segundos?"
+puts "Minha idade equivale a " + (60*60*24*365*20).to_s + " segundos", ""
+
+puts "Quantos chocolates você pretende comer na vida?" 
+puts "Nos próximos 60 anos, pretendo comer " + (3*12*60).to_s + " chocolates", ""
+
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts "Minha idade é de " + ((1232*1000000)/60/60/24/365).to_s + " anos"
+
