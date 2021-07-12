80bee7de7817621d9a1fe8dfcf9861dac7db7c79
Leonardo Silva <leonardo.sbarbosa2801@gmail.com>
2021-07-12 15:17:24 -0300

    Exercicio 03 - corrigindo float e otimizando saida

--- 03-exercicio.rb
+++ 03-exercicio.rb
@@ -6,7 +6,7 @@ totalHoras = totalDias*24
 return totalHoras
 end
 
-puts calculaHorasEmQtdeAnos(2).to_s + " horas", ""
+puts calculaHorasEmQtdeAnos(2).to_s + " horas = 2 anos", ""
 
 
 puts "2. retornar a quantidade de minutos que tem em uma quantidade de décadas, que será um valor informado para a função"
@@ -18,7 +18,7 @@ totalMinutos = totalAnos * 365 * 24 * 60
 return totalMinutos
 end
 
-puts calculaMinutosEmQtdeDecadas(1).to_s + " minutos", ""
+puts calculaMinutosEmQtdeDecadas(1).to_s + " minutos = 1 década", ""
 
 
 puts "3. retornar a idade em valor de segundos, sendo que a idade deve ser informada para a função"
@@ -28,15 +28,17 @@ totalDias = idade * 365
 totalSegundos = totalDias * 60 * 60 * 24
 end
 
-puts calculaIdadeEmSegundos(20).to_s + " segundos", ""
+puts calculaIdadeEmSegundos(20).to_s + " segundos = 20 anos", ""
 
 
 puts "4. retornar a quantidade de anos que tem em uma quantidade de segundos, que será um valor informado para a função"
 
 def calculaAnosEmQtdeSegundos(qtdeSegundos)
-    totalAnos = qtdeSegundos/365/60/60/24.0
+    totalAnos = qtdeSegundos.to_f/365/60/60/24
     return totalAnos
 end
 
 segundosEmUmAno = 31536000 # Quantidade de segundos em 1 ano
-puts calculaAnosEmQtdeSegundos(segundosEmUmAno*3).to_s + " anos", "" # Passa por parâmetro a quantidade de segundos em 3 anos
+segundosEmTresAnos = segundosEmUmAno * 3 # Quantidade de segundos em 3 anos
+
+puts calculaAnosEmQtdeSegundos(segundosEmTresAnos).to_s + " anos possui " + (segundosEmTresAnos).to_s + " segundos", ""
