9b5c6dab7e1901201a025a61383f342560bc4ae4
Leonardo Silva <leonardo.sbarbosa2801@gmail.com>
2021-07-16 17:55:40 -0300

    Exercicio 10 - parte 1 e parte 2

---
+++ 10-exercicio-parte-1.rb
@@ -0,0 +1,10 @@
+for x in (1..10) do
+    for y in (1..10) do
+        if y == 1 || y == 10 || x == 1 || x == 10
+        print " * "    
+        else
+        print " x "
+        end        
+    end
+    puts
+end
---
+++ 10-exercicio-parte-2.rb
@@ -0,0 +1,6 @@
+for x in (1..10) do
+    for y in (1..x) do
+        print "*"
+    end
+    puts
+end
